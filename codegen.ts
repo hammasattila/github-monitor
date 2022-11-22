183b0df8715ae9b5c5dc5c78b7aab7ec509a1f55
hammasattila <hammasattila@outlook.com>
2022-11-22 11:11:03 +0000

    Fixed codegen schem url.

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-	schema: 'schema.graphql',
+	schema: 'https://docs.github.com/public/schema.docs.graphql',
 	documents: ['src/**/*.graphql'],
 	ignoreNoDocuments: true,
 	generates: {
